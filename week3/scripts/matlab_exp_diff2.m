f08f784f2eb76ad0e2afea166d777d37a868b734
Brian Crafton <crafton.b@husky.neu.edu>
2018-02-16 00:57:46 -0500

    matlab stuff

--- week3/scripts/matlab_exp_diff2.m
+++ week3/scripts/matlab_exp_diff2.m
@@ -2,6 +2,7 @@
 M = dlmread('../data/diff2-50-0.1.csv');
 
 x = M(:,1);
-y1 = movmin(M(:,2), 100)
-y2 = movmin(M(:,3), 100)
+y1 = movmin(M(:,2), 100);
+y2 = movmin(M(:,3), 100);
 
+plot(x, [y1, y2], 'b');

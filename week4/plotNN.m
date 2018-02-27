0d0e095b47634e7863e3e9fcd6741e7168e5bbeb
Brian Crafton <crafton.b@husky.neu.edu>
2018-02-27 00:37:11 -0500

    pushing script and doras modified data

--- week4/plotNN.m
+++ week4/plotNN.m
@@ -18,6 +18,78 @@ M = dlmread("./data/ex2/tek0001ALL.csv");
 x = M(:, 1);
 y = M(:, 2);
 
-plot(x, y);
+# plot(x, y);
+
+############
+
+M = dlmread("./data/neuron_recordings/neuron_spike_Iin_0.001nA.csv");
+
+x = M(:, 1);
+y = M(:, 2);
+
+# plot(x, y);
+
+############
+
+M = dlmread("./data/neuron_recordings/neuron_spike_Iin_0.01nA.csv");
+
+x = M(:, 1);
+y = M(:, 2);
+
+# plot(x, y);
+
+############
+
+M = dlmread("./data/neuron_recordings/neuron_spike_Iin_0.1nA.csv");
+
+x = M(:, 1);
+y = M(:, 2);
+
+# plot(x, y);
+
+############
+
+M = dlmread("./data/neuron_recordings/neuron_spike.csv");
+
+x = M(:, 1);
+y = M(:, 2);
+
+# plot(x, y);
+
+############
+
+M = dlmread("./data/neuron_recordings/neuron_spike_Iin_1n.csv");
+
+x = M(:, 1);
+y = M(:, 2);
+
+# plot(x, y);
+
+############
+
+M = dlmread("./data/neuron_recordings/neuron_spike_Iin_10nA.csv");
+
+x = M(:, 1);
+y = M(:, 2);
+
+# plot(x, y);
+
+############
+
+M = dlmread("./data/neuron_recordings/neuron_spike_Iin_18nA.csv");
+
+x = M(:, 1);
+y = M(:, 2);
+
+# plot(x, y);
+
+############
+
+M = dlmread("./data/neuron_recordings/neuron_spike_Iin_50nA.csv");
+
+x = M(:, 1);
+y = M(:, 2);
+
+# plot(x, y);
 
 ############
